4dbfdab67084d1ba4e4696c9ad9479614da3f31e
hamzamullan <80555783+hamzamullan@users.noreply.github.com>
2022-08-16 14:05:12 -0600

    Add files via upload

--- main.js
+++ main.js
@@ -1,11 +1,16 @@
 function startClassification(){
-    navigator.mediaDevices.getUserMedia({ audio: true });
+    navigator.mediaDevices.getUserMedia({ audio: true, video:false });
     classifier = ml5.soundClassifier('https://teachablemachine.withgoogle.com/models/9WJzia9G8/model.json', modelReady);
 }
 
 function modelReady(){
     classifier.classify( gotResults);
 }
+
+var dog=0;
+var cat=0;
+var lion=0;
+var cow=0;
 
 function gotResults( error, results){
     if (error) {
@@ -17,33 +22,26 @@ function gotResults( error, results){
         random_number_b = Math.floor(Math.random() * 255) + 1;
 
         document.getElementById("result_label").innerHTML = 'I can hear - ' + results[0].label;
-        document.getElementById("result_confidence").innerHTML = 'Accuracy - ' + (results[0].confidence*100).toFixed(2) + "%";
+        document.getElementById("result_count").innerHTML = 'Detected dog -  ' +dog+ 'Detected cat - ' + at+ 'Detected Lion - ' +lion + 'Detected cow - ' +cow;
         document.getElementById("result_label").style.color = "rgb("+random_number_r+","+random_number_g+","+random_number_b+")";
-        document.getElementById("result_confidence").style.color = "rgb("+random_number_r+","+random_number_g+","+random_number_b+")";
+        document.getElementById("result_count").style.color = "rgb("+random_number_r+","+random_number_g+","+random_number_b+")";
 
-        img = document.getElementById('dog')
-        img1 = document.getElementById('cat')
-        img2 = document.getElementById('lion')
-        img3 = document.getElementById('cow')
+        img = document.getElementById('animal_image')
 
     if (results[0].label == "bark"){
         img.src = 'download.png';
-        img1.src = 'download (1).png';
-        img2.src = 'download (2).png';
-        img3.src = 'download (3).png';
+        dog=dog+1;
         } else if (results[0].label == "meow"){
-            img.src = 'download.png';
-            img1.src = 'download (1).png';
-            img2.src = 'download (2).png';
-            img3.src = 'download (3).png';
+            img.src = 'download (1).png';
+            cat=cat+1
         } else if (results[0].label == "roar"){
-            img.src = 'download.png';
-            img1.src = 'download (1).png';
-            img2.src = 'download (2).png';
-            img3.src = 'download (3).png';
+            img.src = 'download (3).png';
+            lion=lion+1
+        } else if (results[0].label == "moo"){
+
+            img.src = 'download (2).png';
+            cow=cow+1
         } else{
-            img.src = 'download.png';
-            img1.src = 'download (1).png';
-            img2.src = 'download (2).png';
-            img3.src = 'download (3).png';
-        }}};
+            img.src='we-listen.gif';
+        }
+    }};
