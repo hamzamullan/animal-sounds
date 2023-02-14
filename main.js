ea2dbe76d63c7ec013d223d62e7d84de826d5d61
hamzamullan <80555783+hamzamullan@users.noreply.github.com>
2023-02-13 17:51:12 -0700

    Add files via upload

--- main.js
+++ main.js
@@ -1,10 +1,10 @@
 function startClassification(){
     navigator.mediaDevices.getUserMedia({ audio: true, video:false });
-    classifier = ml5.soundClassifier('https://teachablemachine.withgoogle.com/models/9WJzia9G8/model.json', modelReady);
+    classifier = ml5.soundClassifier('https://teachablemachine.withgoogle.com/models/u02fOYRBx/model.json',{ probabilityThreshold: 0.7 } , modelReady);
 }
 
 function modelReady(){
-    classifier.classify( gotResults);
+    classifier.classify(gotResults);
 }
 
 var dog=0;
@@ -22,25 +22,25 @@ function gotResults( error, results){
         random_number_b = Math.floor(Math.random() * 255) + 1;
 
         document.getElementById("result_label").innerHTML = 'I can hear - ' + results[0].label;
-        document.getElementById("result_count").innerHTML = 'Detected dog -  ' +dog+ 'Detected cat - ' + at+ 'Detected Lion - ' +lion + 'Detected cow - ' +cow;
+        document.getElementById("result_count").innerHTML = 'Detected dog -  ' +dog+ 'Detected cat - ' + cat+ 'Detected Lion - ' +lion + 'Detected cow - ' +cow;
         document.getElementById("result_label").style.color = "rgb("+random_number_r+","+random_number_g+","+random_number_b+")";
         document.getElementById("result_count").style.color = "rgb("+random_number_r+","+random_number_g+","+random_number_b+")";
 
         img = document.getElementById('animal_image')
 
-    if (results[0].label == "bark"){
+    if (results[0].label == "Barking"){
         img.src = 'download.png';
-        dog=dog+1;
-        } else if (results[0].label == "meow"){
+        dog =  dog + 1;
+        } else if (results[0].label == "Meowing"){
             img.src = 'download (1).png';
-            cat=cat+1
-        } else if (results[0].label == "roar"){
+            cat = cat + 1
+        } else if (results[0].label == "Roar"){
             img.src = 'download (3).png';
-            lion=lion+1
-        } else if (results[0].label == "moo"){
+            lion = lion + 1
+        } else if (results[0].label == "Mooing"){
 
             img.src = 'download (2).png';
-            cow=cow+1
+            cow = cow + 1
         } else{
             img.src='we-listen.gif';
         }
